406bc2568a83db30701cdbe5d0b268c5187ea670
Elie-Schooley <elieschooley97@gmail.com>
2020-12-06 19:59:26 -0700

    Add files via upload

--- src/client.py
+++ src/client.py
@@ -1,5 +1,6 @@
-from domain import client,menu,base_message,parser,user
+from domain import client, menu, base_message, parser, user
 
 #TODO, make this your main file
 if __name__ == "__main__":
-    pass
+    menu = menu.Menu(host=input("host"), port=input("port"))
+    menu.run_menu()
